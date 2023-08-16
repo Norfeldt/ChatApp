6c20998a64fdac49d60aecea56ecaf55eb5da0f8
Lasse Nørfeldt <lasse@norfeldt.com>
2023-08-16 13:58:48 +0200

    ✨ ➕ add navigation

--- index.js
+++ index.js
@@ -4,8 +4,9 @@
 
 import {AppRegistry} from 'react-native';
 import { PaperProvider } from 'react-native-paper'
+import 'react-native-gesture-handler'; // https://reactnavigation.org/docs/stack-navigator
 
-import App from './App';
+import { App } from './App';
 import {name as appName} from './app.json';
 
 export default function Main() {
