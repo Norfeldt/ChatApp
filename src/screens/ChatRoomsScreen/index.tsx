6a52e628b8587f31607a2ded6a4feb75fb160111
Lasse Nørfeldt <lasse@norfeldt.com>
2023-08-16 13:58:48 +0200

    ✨ ➕ add navigation

---
+++ src/screens/ChatRoomsScreen/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View } from 'react-native'
+
+import { Text } from 'react-native-paper'
+
+export function ChatRoomsScreen() {
+  return (
+    <View>
+      <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 16 }}>
+        ChatRoomsScreen
+      </Text>
+    </View>
+  )
+}
