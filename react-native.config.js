3fb2e36e4e4b761c5f4dce5ad4b82bc54ec6aab8
Lasse Nørfeldt <lasse@norfeldt.com>
2023-08-16 11:34:48 +0200

    ➕ add vector icons

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dependencies: {
+    // Note: you need to recompile your project after adding new fonts.
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null,
+      },
+    },
+  },
+};
