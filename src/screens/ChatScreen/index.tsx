344e12c286eb79f7fa81fb01c09030b1b4ab6b17
Lasse Nørfeldt <lasse@norfeldt.com>
2023-08-16 13:58:48 +0200

    ✨ ➕ add navigation

---
+++ src/screens/ChatScreen/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View } from 'react-native'
+
+import { Text } from 'react-native-paper'
+
+export function ChatScreen() {
+  return (
+    <View>
+      <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 16 }}>
+        ChatScreen
+      </Text>
+    </View>
+  )
+}
