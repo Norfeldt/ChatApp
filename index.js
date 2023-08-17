afd46fb747e7569c6e795716dab78b1e212b03f4
Lasse Nørfeldt <lasse@norfeldt.com>
2023-08-17 23:58:37 +0200

    ✨ implement Chat feature

--- index.js
+++ index.js
@@ -5,18 +5,20 @@
 import { AppRegistry } from 'react-native'
 import { PaperProvider } from 'react-native-paper'
 import { KeyboardProvider } from 'react-native-keyboard-controller'
-import 'react-native-gesture-handler' // https://reactnavigation.org/docs/stack-navigator
+import { GestureHandlerRootView } from 'react-native-gesture-handler' // https://reactnavigation.org/docs/stack-navigator
 
 import { App } from './App'
 import { name as appName } from './app.json'
 
 export default function Main() {
   return (
-    <PaperProvider>
-      <KeyboardProvider>
-        <App />
-      </KeyboardProvider>
-    </PaperProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <PaperProvider>
+        <KeyboardProvider>
+          <App />
+        </KeyboardProvider>
+      </PaperProvider>
+    </GestureHandlerRootView>
   )
 }
 
