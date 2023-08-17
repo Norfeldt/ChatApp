945220d698770bb3213a4d1ef4c38a378626601b
Lasse Nørfeldt <lasse@norfeldt.com>
2023-08-17 12:48:29 +0200

    ✨ list chatrooms Fixes #5

--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,27 @@
 declare module '@env' {
-  export const WEB_CLIENT_ID: string;
-  export const IOS_CLIENT_ID: string;
+  export const WEB_CLIENT_ID: string
+  export const IOS_CLIENT_ID: string
+}
+
+type ChatRoom = {
+  name: string
+  description: string
+  lastMessageTimestamp: number
+}
+
+type Message = {
+  senderId: string
+  text: string
+  timestamp: number
+}
+
+type RealTimeDatabase = {
+  chatRooms: {
+    [key: string]: ChatRoom
+  }
+  messages: {
+    [key: string]: {
+      [key: string]: Message
+    }
+  }
 }
