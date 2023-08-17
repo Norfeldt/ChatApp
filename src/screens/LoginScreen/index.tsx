98e493ddd7fa8cbea640ea6849556a73f4774aa1
Lasse Nørfeldt <lasse@norfeldt.com>
2023-08-17 10:07:50 +0200

    ✨ add Google Login with navigation

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -1,13 +1,49 @@
 import React from 'react'
-import { View } from 'react-native'
-import { Text } from 'react-native-paper'
+import { View, StyleSheet, Alert } from 'react-native'
+import { Button } from 'react-native-paper'
+import auth from '@react-native-firebase/auth'
+import { GoogleSignin } from '@react-native-google-signin/google-signin'
+import { WEB_CLIENT_ID, IOS_CLIENT_ID } from '@env'
+
+GoogleSignin.configure({
+  offlineAccess: true,
+  scopes: ['profile', 'email'],
+  webClientId: WEB_CLIENT_ID,
+  iosClientId: IOS_CLIENT_ID,
+  googleServicePlistPath: 'GoogleService-Info.plist',
+  forceCodeForRefreshToken: true,
+})
 
 export function LoginScreen() {
+  const onGoogleButtonPress = React.useCallback(async () => {
+    try {
+      const { idToken } = await GoogleSignin.signIn()
+      if (!idToken) {
+        throw new Error('Failed to get ID token')
+      }
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken)
+      return auth().signInWithCredential(googleCredential)
+    } catch (error: any) {
+      Alert.alert(
+        'Failed to sign in with Google',
+        error.message ?? 'Please try again later or contact support'
+      )
+    }
+  }, [])
+
   return (
-    <View>
-      <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 16 }}>
-        LoginScreen
-      </Text>
+    <View style={styles.container}>
+      <Button mode="contained" onPress={() => onGoogleButtonPress()}>
+        Sign in with Google
+      </Button>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
