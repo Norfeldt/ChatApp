ac0de53c4bf8615a37af8c0f6dee6b61fb970bc4
Lasse Nørfeldt <lasse@norfeldt.com>
2023-08-31 23:10:47 +0200

    🔨 add check for critical gitignore files

--- scripts/check-versions.sh
+++ scripts/check-versions.sh
@@ -5,25 +5,28 @@ set -e
 
 echo "installed version (expected version)"
 
-echo "Node.js: $(node --version) (v$(grep '^nodejs ' .tool-versions | cut -d ' ' -f 2))"
+printf "Node.js: $(node --version) (v$(grep '^nodejs ' .tool-versions | cut -d ' ' -f 2)): "
 if [[ "$(node --version | sed 's/v//')" != "$(grep '^nodejs ' .tool-versions | cut -d ' ' -f 2)" ]]; then
   echo "Error: Node.js version does not match .tool-versions"
   exit 1
 fi
+echo "✅"
 
-echo "yarn: $(yarn --version) ($(grep '^yarn ' .tool-versions | cut -d ' ' -f 2))"
+printf "yarn: $(yarn --version) ($(grep '^yarn ' .tool-versions | cut -d ' ' -f 2)): "
 if [[ "$(yarn --version)" != "$(grep '^yarn ' .tool-versions | cut -d ' ' -f 2)" ]]; then
   echo "Error: Yarn version does not match .tool-versions"
   exit 1
 fi
+echo "✅"
 
 installed_ruby_version=$(ruby --version | awk '{print $2}')
 expected_ruby_version=$(grep '^ruby ' .tool-versions | cut -d ' ' -f 2)
 
-echo "ruby: $installed_ruby_version ($expected_ruby_version)"
+printf "ruby: $installed_ruby_version ($expected_ruby_version): "
 if [[ "${installed_ruby_version%.*}" != "${expected_ruby_version%.*}" ]]; then
   echo "Error: Ruby version does not match .tool-versions"
   exit 1
 fi
+echo "✅"
 
 echo "All versions match .tool-versions"
