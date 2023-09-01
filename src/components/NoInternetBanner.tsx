faad4c10038b3c182acd11c47cf6a0b2c9bf6820
Lasse Nørfeldt <lasse@norfeldt.com>
2023-09-01 12:17:57 +0200

    🐛 fix iPhone NoInterNetBanner styling

--- src/components/NoInternetBanner.tsx
+++ src/components/NoInternetBanner.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { AppState, NativeModules, Platform } from 'react-native'
+import { AppState, NativeModules, Platform, View } from 'react-native'
 import NetInfo, { NetInfoState } from '@react-native-community/netinfo'
 import { Banner } from 'react-native-paper'
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export function NoInterNetBanner() {
   const [visible, setVisible] = React.useState(false)
+  const inserts = useSafeAreaInsets()
 
   React.useEffect(() => {
     const subAppState = AppState.addEventListener(
@@ -45,6 +47,9 @@ export function NoInterNetBanner() {
       icon={({ size }) => (
         <MaterialIcons name="signal-wifi-off" size={size} color="#888" />
       )}
+      style={{
+        marginTop: visible ? inserts.top : 0,
+      }}
     >
       No network connection. This may cause some features to be unavailable and
       unexpected behavior.
